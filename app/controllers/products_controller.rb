9206de3365fa997bb0f202b4e195859f3f8ade8e
Nic <nic.ehmayer92@gmail.com>
2018-03-05 02:15:30 +0000

    Product show page now shows overall rating

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -10,6 +10,16 @@ class ProductsController < ApplicationController
     if current_user
       @user_id = current_user.id
     end
+    @overall_rating = overall_rating
+  end
+
+  def overall_rating
+    total = 0
+    amount = @product.ratings.length
+    @product.ratings.each do |r|
+      total += r.rating.to_i
+    end
+    return total / amount
   end
 
 end
