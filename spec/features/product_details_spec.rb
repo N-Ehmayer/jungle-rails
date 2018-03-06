0a9e891002eb715454fc509fa49929cbd2b3da96
Nic <nic.ehmayer92@gmail.com>
2018-03-06 22:47:11 +0000

    Added test to check to check if clicking on product properly redirects to product detail page

---
+++ spec/features/product_details_spec.rb
@@ -0,0 +1,32 @@
+require 'rails_helper'
+
+RSpec.feature "Visitor navigates to product page", type: :feature, js: true do
+
+  before :each do
+    @category = Category.create! name: 'Apparel'
+
+    10.times do |n|
+      @category.products.create!(
+        name:  Faker::Hipster.sentence(3),
+        description: Faker::Hipster.paragraph(4),
+        image: open_asset('apparel1.jpg'),
+        quantity: 10,
+        price: 64.99
+      )
+    end
+  end
+
+  scenario "They see a products show page" do
+    #ACT
+    visit root_path
+    first('article.product').find_link('Details').click
+
+    #Debugger
+    save_screenshot
+
+    #Verify
+    expect(page).to have_current_path(product_path(Product.last))
+
+  end
+
+end
